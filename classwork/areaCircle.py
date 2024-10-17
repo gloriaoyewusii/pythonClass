13dac8c392ada1c69fc401f47ccd39964c799035
gloryacodes <gloriaoyewusii@gmail.com>
2024-10-17 19:10:58 +0100

    classwork folder updated

---
+++ classwork/areaCircle.py
@@ -0,0 +1,4 @@
+
+radius = int(input("Enter the radius: "))
+areaCircle = 3.142 * radius
+print((areaCircle))
---
+++ classwork/inversenumber.py
@@ -0,0 +1,26 @@
+number = int(input("Enter number: "))
+last_number = number % 10
+test_number = number // 10
+second_number = test_number % 10
+first_number = test_number // 10
+print("The inverse number is: ", last_number, second_number, first_number)
+
+odd_numbers = 0
+even_numbers = 0
+
+if (last_number % 2 != 0):
+	odd_numbers+=1
+if (second_number % 2 != 0):
+	odd_numbers+=1
+if (first_number % 2 != 0):
+	odd_numbers+=1
+if (last_number % 2 == 0):
+	even_numbers+=1
+if (second_number % 2 == 0):
+	even_numbers+=1
+if (first_number % 2 == 0):
+	even_numbers+=1
+print("Odd number is: ", odd_numbers)
+print("Even number is: ", even_numbers)
+
+
