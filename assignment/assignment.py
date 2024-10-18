fa686cf3c986bdb6f271f39e229244865a1636af
gloryacodes <gloriaoyewusii@gmail.com>
2024-10-18 07:58:03 +0100

    deleted unnecessary files

--- assignment/assignment.py
+++
@@ -1,4 +0,0 @@
-assignment
-
-rating = input('Enter an integer rating between 1 and 10')
-print(rating)
--- assignment/homeassignment.py
+++
@@ -1,4 +0,0 @@
-ratingAssignment
-
-rating = input('Enter an integer rating between 1 and 10')
-print(rating)
