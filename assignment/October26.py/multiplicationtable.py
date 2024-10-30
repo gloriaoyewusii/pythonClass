47c80e8a21187a6145acefe26af3678049bc47db
gloryacodes <gloriaoyewusii@gmail.com>
2024-10-30 22:37:56 +0100

    11th assignment update

---
+++ assignment/October26.py/multiplicationtable.py
@@ -0,0 +1,5 @@
+for multipliedNumber in range (1, 10, 1):
+    for number in range(1, 9, 1):
+        result = multipliedNumber * number
+        print(f"{number} {multipliedNumber} {result}")
+print(  )
