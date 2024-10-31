fd6118f34080f0d045da813f53a78e61ae62ec38
gloryacodes <gloriaoyewusii@gmail.com>
2024-10-31 06:53:41 +0100

    12th assignment

---
+++ assignment/October26.py/decimalplaces.py
@@ -0,0 +1,15 @@
+Number 1("Input floating number");
+        double num1 = input.nextDouble();
+        System.out.println("Input floating number");
+        double num2 = input.nextDouble();
+        System.out.println(String.format("Number: %.3f", num1));
+        System.out.println(String.format("Number: %.3f", num2));
+
+                if(num1 == num2){
+            System.out.println("They are the same");
+        }
+        else{
+            System.out.println("They are different");
+        }
+        }
+}
