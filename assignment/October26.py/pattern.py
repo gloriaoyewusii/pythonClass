31e85d20927f89aa8e5927b2edf00a15c76cfd80
gloryacodes <gloriaoyewusii@gmail.com>
2024-10-31 08:22:50 +0100

    the 13th

---
+++ assignment/October26.py/pattern.py
@@ -0,0 +1,12 @@
+rowNumber = 6
+for rowNumber in range(1, rowNumber, 1):
+	print("*")
+for rowNumber in range(1, rowNumber, 1):
+	print("\t*")
+for rowNumber in range(1, rowNumber, 1):
+	print("\t\t*")
+for rowNumber in range(1, rowNumber, 1):
+	print("\t\t\t*")
+for rowNumber in range(1, rowNumber, 1):
+	print("\t\t\t\t*")
+
